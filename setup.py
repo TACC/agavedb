764448989bfdf9f828087681420db4662324b2eb
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-02-19 07:21:48 -0600

    needed separate environment for 2.7 to accomodate wsgiref dependency in 2.7.x

--- setup.py
+++ setup.py
@@ -36,7 +36,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
         'Environment :: Other Environment',
         'Topic :: Software Development :: Libraries :: Python Modules'
